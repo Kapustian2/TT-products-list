ec00cac00eea1b2c423180290c7f0003f6c7030d
Kapustian2 <123888044+Kapustian2@users.noreply.github.com>
2024-02-29 22:21:45 +0600

    Implement products list

---
+++ src/components/products-list.tsx
@@ -0,0 +1,28 @@
+import { Product } from "../types/product";
+
+export const ProductList = (products: Product[]) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Название</th>
+            <th>Цена</th>
+            <th>Бренд</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.product}</td>
+              <td>{product.price}</td>
+              <td>{product.brand}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
---
+++ src/types/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  id: string;
+  price: number;
+  product: string;
+  brand: string | null;
+}
